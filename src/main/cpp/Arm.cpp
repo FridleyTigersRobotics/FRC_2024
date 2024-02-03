74d4641ace4992267dd29ab8d250108d5de89a12
fhsrobotics2227 <tigers.robotics@gmail.com>
2024-02-03 13:33:53 -0600

    Did silly arm math :D

--- src/main/cpp/Arm.cpp
+++ src/main/cpp/Arm.cpp
@@ -11,34 +11,40 @@ m_ArmPosition = DesiredPosition;
 
 void Arm::updateArm()
 {
+    double ArmAngle = 0;
+
  switch (m_ArmPosition)
     {
         case (GROUND_PICKUP):
         {
-
+            ArmAngle = 0;
             break;
         }
         case (SOURCE):
         {
-
+            ArmAngle = 0.4;
             break;
         }
         case (SPEAKER):
         {
-
+            ArmAngle = 0;
             break;
         }
         case (AMP):
         {
-
+            ArmAngle = 0.5;
             break;
         }
         case (TRAP):
         {
-
+            ArmAngle = 1.0;
             break;
         }
     }
 
-
+//PIDdly thing
+const auto ArmControllOutput = m_ArmPIDController.Calculate(
+      units::radian_t{m_ArmEncoder.GetDistance()}, units::radian_t{ArmAngle});
+m_ArmMotorLeft.Set(ArmControllOutput);
+m_ArmMotorRight.Set(ArmControllOutput);
 }
--- src/main/include/Arm.h
+++ src/main/include/Arm.h
@@ -1,7 +1,13 @@
 #include <rev/cansparkmax.h>
 #include <Constants.h>
 #include <frc/DutyCycleEncoder.h>
-
+#include <frc/controller/ProfiledPIDController.h>
+#include <frc/controller/PIDController.h>
+#include <numbers>
+#include <units/angular_velocity.h>
+#include <units/time.h>
+#include <units/velocity.h>
+#include <units/voltage.h>
 typedef enum arm_positon_e
 {
     GROUND_PICKUP,
@@ -26,4 +32,18 @@ frc::DutyCycleEncoder m_ArmEncoder  { ConstantCrap::kArmEncoderDIO };
 //Wrist
 rev::CANSparkMax m_WristMotor {ConstantCrap::kWristMotorID,rev::CANSparkLowLevel::MotorType::kBrushless};
 frc::DutyCycleEncoder m_WristEncoder { ConstantCrap::kWristEncoderDIO };
+
+//PID
+      static constexpr auto kArmAngleVelocity =
+      std::numbers::pi * 1_rad_per_s;  // radians per second
+  static constexpr auto kArmAngleAcceleration =
+      std::numbers::pi * 2_rad_per_s / 1_s;  // radians per second^2  
+     
+      frc::ProfiledPIDController<units::radians> m_ArmPIDController{
+      1.0,
+      0.0,
+      0.0,
+      {kArmAngleVelocity, kArmAngleAcceleration}};
+
+
 };
