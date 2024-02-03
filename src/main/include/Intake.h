848fe3054a545307e24d3120de2b2d458aefd2e9
SillySeagull <paque35928@isd14.org>
2024-02-03 13:33:44 -0600

    Wrist stuff got added to Arm

--- src/main/include/Intake.h
+++ src/main/include/Intake.h
@@ -5,6 +5,6 @@
 
 class Intake
 {
-    void updateIntake (/*Phoenix!??!?!?! Harry Potter reference?????????*/);
+    void updateIntake (/*Phoenix!??!?!?! Harry Potter reference?????????*/); //Yo thats crazy
 rev::CANSparkMax m_IntakeMotor { ConstantCrap::kIntakeMotorcanID,rev::CANSparkLowLevel::MotorType::kBrushless };
 };
