4d02fb6eaf826cc3123444d5cbca0139d30b99dd
fhsrobotics2227 <tigers.robotics@gmail.com>
2024-02-03 12:58:01 -0600

    Yummy switch :P

--- src/main/cpp/Arm.cpp
+++ src/main/cpp/Arm.cpp
@@ -1,10 +1,44 @@
 #include <Arm.h>
 #include <rev/cansparkmax.h>
 //#include <arms> *flexes, cutely*
+void Arm::SetArmPosition (arm_position_t DesiredPosition)
+{
+m_ArmPosition = DesiredPosition;
+
+
+
+}
+
 void Arm::updateArm()
 {
- 
-//Arm :)
+ switch (m_ArmPosition)
+    {
+        case (GROUND_PICKUP):
+        {
+
+            break;
+        }
+        case (SOURCE):
+        {
+
+            break;
+        }
+        case (SPEAKER):
+        {
+
+            break;
+        }
+        case (AMP):
+        {
+
+            break;
+        }
+        case (TRAP):
+        {
+
+            break;
+        }
+    }
 
 
 }
--- src/main/include/Arm.h
+++ src/main/include/Arm.h
@@ -1,9 +1,23 @@
 #include <rev/cansparkmax.h>
 #include <Constants.h>
 #include <frc/DutyCycleEncoder.h>
+
+typedef enum arm_positon_e
+{
+    GROUND_PICKUP,
+    SOURCE,
+    AMP,
+    SPEAKER,
+    TRAP
+} arm_position_t;
+
+
+
 class Arm
 {
-void updateArm (/*Arm? I hove those!*/);
+    arm_position_t m_ArmPosition {GROUND_PICKUP};
+void SetArmPosition (arm_position_t DesiredPosition);
+void updateArm (/*Arm? I have those!*/);
 //Arm
 rev::CANSparkMax m_ArmMotorLeft { ConstantCrap::kArmMotorLeftcanID,rev::CANSparkLowLevel::MotorType::kBrushless };
 rev::CANSparkMax m_ArmMotorRight { ConstantCrap::kArmMotorRightcanID,rev::CANSparkLowLevel::MotorType::kBrushless };
